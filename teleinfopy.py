bb6cb48c694e31909d8778d5cd38bc68f80dfcba
Sylvain A <sysdev.samc@gmail.com>
2018-12-16 09:48:51 +0100

    teleinfopy.py

--- teleinfopy.py
+++ teleinfopy.py
@@ -1,45 +1,21 @@
-#!/usr/bin/env python
+#!/home/pi/venvs/teleinfopy/bin/python2
 # -*- coding: utf-8 -*-
-import os,sys
+
 import begin
 import logging
+import time
+import sdnotify
+import datetime
 
 from teleinfo.teleinfo_serial import Teleinfo_serial
 import paho.mqtt.client as mqtt
 
-
-
-framesTI={
-  'ADCO':'',
-  'OPTARIF':'',
-  'ISOUSC':'',
-  'BASE':'',
-  'HCHC':'0',
-  'HCHP':'0',
-  'EJPHN':'0',
-  'EJPHPM':'0',
-  'BBRHCJB':'0',
-  'BBRHPJB':'0',
-  'BBRHCJW':'0',
-  'BBRHPJW':'0',
-  'BBRHCJR':'0',
-  'BBRHPJR':'0',
-  'PEJP':'0',
-  'PTEC':'',
-  'DEMAIN':'',
-  'IINST':'0',
-  'IINST1':'0',
-  'IINST2':'0',
-  'IINST3':'0',
-  'IMAX':'0',
-  'IMAX1':'0',
-  'IMAX2':'0',
-  'IMAX3':'0',
-  'PMAX':'0',
-  'PAPP':'',
-  'HHPHC':'',
-  'MOTDETAT':'',
-  'PPOT':''}
+framesHCTI={
+  'HCHC':'O',
+  'HCHP':'O',
+  'IINST':'O',
+  'IMAX':'O',
+  'PAPP':'O'}
 
 
 def mqtt_publish(ti,mqtt_host, mqtt_user, mqtt_password, mqtt_port):
@@ -48,23 +24,52 @@ def mqtt_publish(ti,mqtt_host, mqtt_user, mqtt_password, mqtt_port):
 
   client.connect(mqtt_host, mqtt_port, 60)
   client.loop_start()
-  client.publish('teleinfo/PAPP', int(ti['PAPP']))
+  papp=int(ti['PAPP'])
+  if papp < 20000 and papp >= 0:
+    logging.info("PAPP {0}".format(papp))
+    client.publish('teleinfo/PAPP', papp)
+
   hc=str(int(ti['HCHC']))
-  hp=str(int(ti['HCHP']))
   client.publish('teleinfo/HC', hc[:5])
+  hp=str(int(ti['HCHP']))
   client.publish('teleinfo/HP', hp[:5])
+  iinst=str(int(ti['IINST']))
+  client.publish('teleinfo/IINST', iinst)
+  imax=str(int(ti['IMAX']))
+  client.publish('teleinfo/IMAX', imax)
+
+  client.publish('teleinfo/Time', datetime.datetime.now().isoformat())
+
+  time.sleep(1)
+
+  client.loop_stop()
   client.disconnect()
 
 
-@begin.start(config_file='teleinfo.cfg')
+@begin.start(config_file='/etc/teleinfo.cfg')
 @begin.logging
-def default(device='/dev/ttyAMA0',mode='mqtt',mqtt_host, mqtt_user, mqtt_password, mqtt_port):
+def default(device='/dev/ttyAMA0',
+            mode='mqtt',
+            mqtt_host='localhost',
+            mqtt_user='user',
+            mqtt_password='password',
+            mqtt_port=1883):
   logging.info("device : %s",device)
   serial_device=Teleinfo_serial(port=device)
-  data=serial_device.read(framesTI)
+  n = sdnotify.SystemdNotifier()
+  n.notify("READY=1")
+  try:
+    while True:
+      n.notify("WATCHDOG=1")
+      time.sleep(2)
+      data=serial_device.read(framesHCTI)
+      if data is False:
+        continue
+      if mode == 'mqtt':
+        mqtt_publish(data,mqtt_host, mqtt_user, mqtt_password, mqtt_port)
+      time.sleep(20)
+  except KeyboardInterrupt:
+    pass
   serial_device.close()
-  if mode == 'mqtt':
-    mqtt_publish(data,mqtt_host, mqtt_user, mqtt_password, mqtt_port)
-  logging.info("insertion des données")
 
 
